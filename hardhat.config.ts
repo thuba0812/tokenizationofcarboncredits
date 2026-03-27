db1ece22f90b495e3adb6008ba7276784c5d27b7
zang <giangtnc23416@st.uel.edu.vn>
2026-03-27 18:42:44 +0700

    chore: restore hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      evmVersion: "cancun",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
+};
+
+export default config;
